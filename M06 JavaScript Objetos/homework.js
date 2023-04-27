3e3efb341e9a483729551bdc16b6f402a480dca0
Josue-Fernandez <fernandezt.josue@gmail.com>
2023-04-26 23:38:20 -0400

    mod 6

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -7,6 +7,10 @@ function crearGato(nombre, edad) {
    // La propiedad "meow" será una función que retorne el string: "Meow!".
    // Retornar el objeto.
    // Tu código:
+
+   var nuevo ={nombre: nombre , edad:edad, meow: function(){ return('Meow!')}};
+   
+   return nuevo;
 }
 
 function nuevoUsuario(nombre, email, password) {
@@ -14,6 +18,8 @@ function nuevoUsuario(nombre, email, password) {
    // Este debe tener las propiedades: "nombre", "email" y "password" con sus respectivos valores.
    // Retornar el objeto.
    // Tu código:
+   var nuevo = {nombre: nombre , email: email , password: password};
+   return nuevo;
 }
 
 function agregarPropiedad(objeto, propiedad) {
@@ -22,6 +28,7 @@ function agregarPropiedad(objeto, propiedad) {
    // Esta propiedad será igual al valor `null`.
    // Retornar el objeto.
    // Tu código:
+   
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -29,6 +36,7 @@ function invocarMetodo(objeto, metodo) {
    // Esta propiedad contiene una función en su interior. Debes invocarla/ejecutarla.
    // [NOTA]: no necesitar retornar nada.
    // Tu código:
+   
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
